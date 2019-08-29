f6a8dc086fdba30a8fdf0dd8020bffd4a356c40a
Everett Lee-Wuollet and Franklin Waggoner <franklin@>
2019-08-28 19:50:27 -0700

    first commit for calculator

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+//adding
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
+
+
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtract(number1, number2);
+alert(result);
+
+
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1, number2);
+alert(result);
+
+
+
+var division = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = division(number1, number2);
+alert(result);
+
+
+var temp = function(number1) {
+  return number1 - 32 * 5/9;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var result = temp(number1);
+alert(result);
